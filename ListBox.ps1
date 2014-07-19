7aef77feff218fdbfacd2055a025e8e72546efa9
Dave Wyatt <dlwyatt115@gmail.com>
2014-07-18 20:35:16 -0400

    Added original versions from TechNet.

---
+++ ListBox.ps1
@@ -0,0 +1,57 @@
+# Original example posted at http://technet.microsoft.com/en-us/library/ff730949.aspx
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+
+$objForm = New-Object System.Windows.Forms.Form 
+$objForm.Text = "Select a Computer"
+$objForm.Size = New-Object System.Drawing.Size(300,200) 
+$objForm.StartPosition = "CenterScreen"
+
+$objForm.KeyPreview = $True
+$objForm.Add_KeyDown({if ($_.KeyCode -eq "Enter") 
+    {$x=$objListBox.SelectedItem;$objForm.Close()}})
+$objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") 
+    {$objForm.Close()}})
+
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Size(75,120)
+$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton.Text = "OK"
+$OKButton.Add_Click({$x=$objListBox.SelectedItem;$objForm.Close()})
+$objForm.Controls.Add($OKButton)
+
+$CancelButton = New-Object System.Windows.Forms.Button
+$CancelButton.Location = New-Object System.Drawing.Size(150,120)
+$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+$CancelButton.Text = "Cancel"
+$CancelButton.Add_Click({$objForm.Close()})
+$objForm.Controls.Add($CancelButton)
+
+$objLabel = New-Object System.Windows.Forms.Label
+$objLabel.Location = New-Object System.Drawing.Size(10,20) 
+$objLabel.Size = New-Object System.Drawing.Size(280,20) 
+$objLabel.Text = "Please select a computer:"
+$objForm.Controls.Add($objLabel) 
+
+$objListBox = New-Object System.Windows.Forms.ListBox 
+$objListBox.Location = New-Object System.Drawing.Size(10,40) 
+$objListBox.Size = New-Object System.Drawing.Size(260,20) 
+$objListBox.Height = 80
+
+[void] $objListBox.Items.Add("atl-dc-001")
+[void] $objListBox.Items.Add("atl-dc-002")
+[void] $objListBox.Items.Add("atl-dc-003")
+[void] $objListBox.Items.Add("atl-dc-004")
+[void] $objListBox.Items.Add("atl-dc-005")
+[void] $objListBox.Items.Add("atl-dc-006")
+[void] $objListBox.Items.Add("atl-dc-007")
+
+$objForm.Controls.Add($objListBox) 
+
+$objForm.Topmost = $True
+
+$objForm.Add_Shown({$objForm.Activate()})
+[void] $objForm.ShowDialog()
+
+$x
---
+++ MultiSelectListBox.ps1
@@ -0,0 +1,72 @@
+# Original example posted at http://technet.microsoft.com/en-us/library/ff730950.aspx
+
+$x = @()
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+
+$objForm = New-Object System.Windows.Forms.Form 
+$objForm.Text = "Data Entry Form"
+$objForm.Size = New-Object System.Drawing.Size(300,200) 
+$objForm.StartPosition = "CenterScreen"
+
+$objForm.KeyPreview = $True
+
+$objForm.Add_KeyDown({if ($_.KeyCode -eq "Enter") 
+    {
+        foreach ($objItem in $objListbox.SelectedItems)
+            {$x += $objItem}
+        $objForm.Close()
+    }
+    })
+
+$objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") 
+    {$objForm.Close()}})
+
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Size(75,120)
+$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton.Text = "OK"
+
+$OKButton.Add_Click(
+   {
+        foreach ($objItem in $objListbox.SelectedItems)
+            {$x += $objItem}
+        $objForm.Close()
+   })
+
+$objForm.Controls.Add($OKButton)
+
+$CancelButton = New-Object System.Windows.Forms.Button
+$CancelButton.Location = New-Object System.Drawing.Size(150,120)
+$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+$CancelButton.Text = "Cancel"
+$CancelButton.Add_Click({$objForm.Close()})
+$objForm.Controls.Add($CancelButton)
+
+$objLabel = New-Object System.Windows.Forms.Label
+$objLabel.Location = New-Object System.Drawing.Size(10,20) 
+$objLabel.Size = New-Object System.Drawing.Size(280,20) 
+$objLabel.Text = "Please make a selection from the list below:"
+$objForm.Controls.Add($objLabel) 
+
+$objListbox = New-Object System.Windows.Forms.Listbox 
+$objListbox.Location = New-Object System.Drawing.Size(10,40) 
+$objListbox.Size = New-Object System.Drawing.Size(260,20) 
+
+$objListbox.SelectionMode = "MultiExtended"
+
+[void] $objListbox.Items.Add("Item 1")
+[void] $objListbox.Items.Add("Item 2")
+[void] $objListbox.Items.Add("Item 3")
+[void] $objListbox.Items.Add("Item 4")
+[void] $objListbox.Items.Add("Item 5")
+
+$objListbox.Height = 70
+$objForm.Controls.Add($objListbox) 
+$objForm.Topmost = $True
+
+$objForm.Add_Shown({$objForm.Activate()})
+[void] $objForm.ShowDialog()
+
+$x
