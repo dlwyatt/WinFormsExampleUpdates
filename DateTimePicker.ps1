aa2dca29aea7d0dbdb3fed759ec15efb1faa3460
Dave Wyatt <dlwyatt115@gmail.com>
2014-07-18 20:45:20 -0400

    DateTimePicker correction
    Had wrong original script copied in the file.

--- DateTimePicker.ps1
+++ DateTimePicker.ps1
@@ -1,72 +1,42 @@
 # Original example posted at http://technet.microsoft.com/en-us/library/ff730942.aspx
 
-$x = @()
-
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
 
-$objForm = New-Object System.Windows.Forms.Form 
-$objForm.Text = "Data Entry Form"
-$objForm.Size = New-Object System.Drawing.Size(300,200) 
+$objForm = New-Object Windows.Forms.Form 
+
+$objForm.Text = "Select a Date" 
+$objForm.Size = New-Object Drawing.Size @(190,190) 
 $objForm.StartPosition = "CenterScreen"
 
 $objForm.KeyPreview = $True
 
-$objForm.Add_KeyDown({if ($_.KeyCode -eq "Enter") 
-    {
-        foreach ($objItem in $objListbox.SelectedItems)
-            {$x += $objItem}
-        $objForm.Close()
-    }
+$objForm.Add_KeyDown({
+    if ($_.KeyCode -eq "Enter") 
+        {
+            $dtmDate=$objCalendar.SelectionStart
+            $objForm.Close()
+        }
     })
 
-$objForm.Add_KeyDown({if ($_.KeyCode -eq "Escape") 
-    {$objForm.Close()}})
+$objForm.Add_KeyDown({
+    if ($_.KeyCode -eq "Escape") 
+        {
+            $objForm.Close()
+        }
+    })
 
-$OKButton = New-Object System.Windows.Forms.Button
-$OKButton.Location = New-Object System.Drawing.Size(75,120)
-$OKButton.Size = New-Object System.Drawing.Size(75,23)
-$OKButton.Text = "OK"
+$objCalendar = New-Object System.Windows.Forms.MonthCalendar 
+$objCalendar.ShowTodayCircle = $False
+$objCalendar.MaxSelectionCount = 1
+$objForm.Controls.Add($objCalendar) 
 
-$OKButton.Add_Click(
-   {
-        foreach ($objItem in $objListbox.SelectedItems)
-            {$x += $objItem}
-        $objForm.Close()
-   })
-
-$objForm.Controls.Add($OKButton)
-
-$CancelButton = New-Object System.Windows.Forms.Button
-$CancelButton.Location = New-Object System.Drawing.Size(150,120)
-$CancelButton.Size = New-Object System.Drawing.Size(75,23)
-$CancelButton.Text = "Cancel"
-$CancelButton.Add_Click({$objForm.Close()})
-$objForm.Controls.Add($CancelButton)
-
-$objLabel = New-Object System.Windows.Forms.Label
-$objLabel.Location = New-Object System.Drawing.Size(10,20) 
-$objLabel.Size = New-Object System.Drawing.Size(280,20) 
-$objLabel.Text = "Please make a selection from the list below:"
-$objForm.Controls.Add($objLabel) 
-
-$objListbox = New-Object System.Windows.Forms.Listbox 
-$objListbox.Location = New-Object System.Drawing.Size(10,40) 
-$objListbox.Size = New-Object System.Drawing.Size(260,20) 
-
-$objListbox.SelectionMode = "MultiExtended"
-
-[void] $objListbox.Items.Add("Item 1")
-[void] $objListbox.Items.Add("Item 2")
-[void] $objListbox.Items.Add("Item 3")
-[void] $objListbox.Items.Add("Item 4")
-[void] $objListbox.Items.Add("Item 5")
-
-$objListbox.Height = 70
-$objForm.Controls.Add($objListbox) 
 $objForm.Topmost = $True
 
-$objForm.Add_Shown({$objForm.Activate()})
-[void] $objForm.ShowDialog()
+$objForm.Add_Shown({$objForm.Activate()})  
+[void] $objForm.ShowDialog() 
 
-$x
+if ($dtmDate)
+    {
+        Write-Host "Date selected: $dtmDate"
+    }
