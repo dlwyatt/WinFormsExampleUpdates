37c36696f9dc98bf31983ca3273014c3806a4fad
Dave Wyatt <dlwyatt115@gmail.com>
2014-07-24 16:34:21 -0400

    Point / Size correction
    Original code was assigning Size objects to the Location property of controls.  This property actually takes a Point object, and while PowerShell was successfully converting Size to Point anyway, it's more correct to use the proper objects in the PowerShell code.

--- ListBox.ps1
+++ ListBox.ps1
@@ -9,7 +9,7 @@ $form.Size = New-Object System.Drawing.Size(300,200)
 $form.StartPosition = "CenterScreen"
 
 $OKButton = New-Object System.Windows.Forms.Button
-$OKButton.Location = New-Object System.Drawing.Size(75,120)
+$OKButton.Location = New-Object System.Drawing.Point(75,120)
 $OKButton.Size = New-Object System.Drawing.Size(75,23)
 $OKButton.Text = "OK"
 $OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
@@ -17,7 +17,7 @@ $form.AcceptButton = $OKButton
 $form.Controls.Add($OKButton)
 
 $CancelButton = New-Object System.Windows.Forms.Button
-$CancelButton.Location = New-Object System.Drawing.Size(150,120)
+$CancelButton.Location = New-Object System.Drawing.Point(150,120)
 $CancelButton.Size = New-Object System.Drawing.Size(75,23)
 $CancelButton.Text = "Cancel"
 $CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
@@ -25,13 +25,13 @@ $form.CancelButton = $CancelButton
 $form.Controls.Add($CancelButton)
 
 $label = New-Object System.Windows.Forms.Label
-$label.Location = New-Object System.Drawing.Size(10,20) 
+$label.Location = New-Object System.Drawing.Point(10,20) 
 $label.Size = New-Object System.Drawing.Size(280,20) 
 $label.Text = "Please select a computer:"
 $form.Controls.Add($label) 
 
 $listBox = New-Object System.Windows.Forms.ListBox 
-$listBox.Location = New-Object System.Drawing.Size(10,40) 
+$listBox.Location = New-Object System.Drawing.Point(10,40) 
 $listBox.Size = New-Object System.Drawing.Size(260,20) 
 $listBox.Height = 80
 
